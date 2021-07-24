Fix gradient error

Optimizer/A3C/Worker.py
@@ -44,7 +44,7 @@ class Worker(Server):  # Optimizer
             if self.step == 0:
                 dumpfile_writer.writerow(experience.keys())
             dumpfile_writer.writerow([
-                self.step, reward, tensor2value(state), action, tensor2value(policy_prob), tensor2value(behavior_prob)
+                self.step, reward, tensor2value(state), action, policy_prob, behavior_prob
             ])
 
         self.step += 1
@@ -76,6 +76,8 @@ class Worker(Server):  # Optimizer
         mu = 1
         for i in range(t):              # mu = pi(A0|S0)/b(A0|S0) * pi(A1|S1)/b(A1|S1)
             mu *= M[i]
+
+        print(mu)
 
         for i in range(t):              # 0, 1
             j = t - 1 - i               # 1, 0
@@ -125,7 +127,7 @@ class Worker(Server):  # Optimizer
 
         heuristic_policy = get_heuristic_policy(net=network, mc=mc, worker=self, time_stamp=time_stamp)
         assert not torch.isnan(heuristic_policy).any(), "Heuristic policy contains Nan value"
-        assert float(torch.sum(heuristic_policy)) == 1, "Heuristic policy is false (sum not equals to 1)"
+        # assert float(torch.sum(heuristic_policy)) == 1, "Heuristic policy is false (sum not equals to 1)"
 
         behavior_policy = (1 - self.alpha_H) * policy + self.alpha_H * heuristic_policy
         action = np.random.choice(self.action_space, p=tensor2value(behavior_policy))
@@ -134,7 +136,7 @@ class Worker(Server):  # Optimizer
         self.buffer.append(
             self.create_experience(
                 state=state_tensor, action=action,
-                policy_prob=policy[action], behavior_prob=behavior_policy[action],
+                policy_prob=tensor2value(policy[action]), behavior_prob=tensor2value(behavior_policy[action]),
                 reward=R
             )
         )

Optimizer/A3C/Worker_method.py
@@ -82,13 +82,15 @@ def extract_state_tensor_v2(worker, net):
 
 
 def charging_time_func(mc=None, net=None, action_id=None, time_stamp=0, theta=0.1):
-    return H_charging_time_func(mc, net, action_id, time_stamp, theta)
+    return H_charging_time_func(mc=mc, net=net, action_id=action_id, time_stamp=time_stamp, theta=theta)
 
 
 # TODO: implement heuristic policy (Nguyen Thanh Long)
 def get_heuristic_policy(net=None, mc=None, worker=None, time_stamp=0):
-    return H_get_heuristic_policy(net, mc, worker, time_stamp)
-
+    # return H_get_heuristic_policy(net=net, mc=mc, worker=worker, time_stamp=time_stamp)
+    H_policy = torch.ones_like(torch.Tensor(worker.action_space)) / worker.nb_action
+    H_policy.requires_grad = False
+    return H_policy
 
 def one_hot(index, size):
     one_hot_vector = np.zeros(size)